39554d868df01c78fabdf5010b3ec3a95ffbe601
Lucas Baker <lucas.jd.baker+1@gmail.com>
2018-05-13 15:04:36 +0900

    Silence all loading messages.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/Users/lucasbaker/Downloads/google-cloud-sdk/bin/gcloud compute ssh lucas_jd_baker@cormorant-001 --command "bash /home/lucas_jd_baker/run_elf.sh"
+/Users/lucasbaker/Downloads/google-cloud-sdk/bin/gcloud compute ssh lucas_jd_baker@cormorant-001 --command "bash /home/lucas_jd_baker/ELF/scripts/elfgames/go/run_ELF.sh"
